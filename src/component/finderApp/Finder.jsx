09fa94a445d9504b36a674c96e90ed3b629bec67
codewithgoodluck <goodluckmordi4@gmail.com>
2022-01-03 23:41:51 +0100

    testing

---
+++ src/component/finderApp/Finder.jsx
@@ -0,0 +1,15 @@
+
+import { Card } from 'react-bootstrap'
+
+const DisplayUser = () => {
+    return (
+        <div className="container">
+            <Card>
+             <Card.Body>This is some text within a card body.</Card.Body>
+            </Card>
+            
+        </div>
+    )
+}
+
+export default DisplayUser
